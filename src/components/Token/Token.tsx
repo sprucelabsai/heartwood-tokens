03418b6a02562be494e4caff2438bebe54f9e3eb
jtini <jeremytinianow@Jeremys-MacBook-Pro-2.local>
2019-07-29 14:27:23 -0600

    improve tokens loop and start adding styles

--- src/components/Token/Token.tsx
+++ src/components/Token/Token.tsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import './Token.scss'
 
 export interface ITokenProps {
     token: Token,
--- src/pages/tokens.tsx
+++ src/pages/tokens.tsx
@@ -2,7 +2,7 @@ import * as React from 'react'
 import Layout from '../layouts/Layout/Layout'
 import Token from '../components/Token/Token'
 import tokensScss from '../../build/js/tokens-scss'
-import './index.scss'
+import './tokens.scss'
 
 interface TokensPageProps {
 	platform?: Platform
@@ -23,29 +23,35 @@ export default class TokensPage extends React.Component<TokensPageProps, {}> {
 
 		return (
 			<Layout>
-				<div className="container debug">
+				<div className="container ">
 					{Object.keys(tokens).map(cat => (
-						<section key={cat}>
-							<h2 className="h1">{cat.split('-').join(' ')}</h2>
-							{Object.keys(tokens[cat]).map(type => (
-								<section key={type}>
-									{!tokens[cat][type].value && <h3>{type}</h3>}
-									{tokens[cat][type].value
-									? <Token token={tokens[cat][type]} platform={platform}/>
-									: Object.keys(tokens[cat][type]).map(item => (
-										<section key={item}>
-											{!tokens[cat][type][item].value && <h4>{item}</h4> }
-											{tokens[cat][type][item].value
-											? <Token token={tokens[cat][type][item]} platform={platform}/>
-											: Object.keys(tokens[cat][type][item]).map(subitem => (
-												<div key={subitem}>
-													<p>{subitem}</p>
-												</div>
-											))}
+						<section className="token-category-section" key={cat}>
+							<h2 className="title-lg tokens-category__title">{cat.split('-').join(' ')}</h2>
+							{Object.keys(tokens[cat]).map(type => {
+								if (!tokens[cat][type].value) {
+									return (
+										<section className="token-type-section" key={type}>
+											<h3 className="tokens-type__title title-sm">{type}</h3>
+											{Object.keys(tokens[cat][type]).map(item => {
+												if (!tokens[cat][type][item].value) {
+													return (
+														<section className="token-item-section" key={item}>
+															<h4 className="tokens-item__title">{item}</h4>
+															{Object.keys(tokens[cat][type][item]).map(subitem => (
+																<div key={subitem}>
+																	<p>{subitem}</p>
+																</div>
+															))}
+														</section>
+													)
+												}
+												return <Token token={tokens[cat][type][item]} platform={platform}/>})
+											}
 										</section>
-									))}
-								</section>
-							))}
+									)
+								}
+								return <Token token={tokens[cat][type]} platform={platform}/>
+							})}
 						</section>
 					))}
 				</div>
