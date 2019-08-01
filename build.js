9088226557fdae9446740b3615484297462aeef9
jtini <jeremytinianow@Jeremys-MacBook-Pro-2.local>
2019-08-01 16:08:04 -0600

    rework figma color styles more sensibly

--- build.js
+++ build.js
@@ -1,210 +1,294 @@
-const StyleDictionary = require('style-dictionary').extend(__dirname + '/config.json');
-const _ = require('lodash')
-const tinycolor = require('tinycolor2')
+const StyleDictionary = require("style-dictionary").extend(
+  __dirname + "/config.json"
+);
+const _ = require("lodash");
+const tinycolor = require("tinycolor2");
 
-const { fileHeader, variablesWithPrefix, sassMultiMap, sassVarName } = require('./formatters')
+const {
+  fileHeader,
+  variablesWithPrefix,
+  sassMultiMap,
+  sassVarName
+} = require("./formatters");
 
 /**
  * Transforms
  */
 StyleDictionary.registerTransform({
-  name: 'name/bem',
-  type: 'name',
+  name: "name/bem",
+  type: "name",
   transformer: (prop, options) => {
     // Get the properties
     const { attributes } = prop;
-    const category = typeof attributes.category !== 'undefined' ? attributes.category : null
-    const type = typeof attributes.type !== 'undefined' ? attributes.type : null
-    const item = typeof attributes.item !== 'undefined' ? attributes.item : null
-    const subitem = typeof attributes.subitem !== 'undefined' ? attributes.subitem : null
-    const state = typeof attributes.state !== 'undefined' ? attributes.state : null
+    const category =
+      typeof attributes.category !== "undefined" ? attributes.category : null;
+    const type =
+      typeof attributes.type !== "undefined" ? attributes.type : null;
+    const item =
+      typeof attributes.item !== "undefined" ? attributes.item : null;
+    const subitem =
+      typeof attributes.subitem !== "undefined" ? attributes.subitem : null;
+    const state =
+      typeof attributes.state !== "undefined" ? attributes.state : null;
 
     // Map categories to prefixes
     const catPrefixes = {
-      color: 'c',
-      'background-color': 'bg',
+      color: "c",
+      "background-color": "bg",
       // 'text-color': 'tc',
-      layer: 'layer',
-      'font-weight': 'fw',
-      'font-size': 'fs',
+      layer: "layer",
+      "font-weight": "fw",
+      "font-size": "fs",
       // 'font-style': 'fstyle',
-      'breakpoint': 'bp',
-      'line-height': 'lh',
-      border: 'border',
-      'border-radius': 'br',
-      dimension: 'dim',
-      size: 'size',
-      layout: 'l',
-      margin: 'm',
-      padding: 'p'
-    }
+      breakpoint: "bp",
+      "line-height": "lh",
+      border: "border",
+      "border-radius": "br",
+      dimension: "dim",
+      size: "size",
+      layout: "l",
+      margin: "m",
+      padding: "p"
+    };
 
     // Reformat the category name to be shorter
-    const newCat = catPrefixes[category] || category
+    const newCat = catPrefixes[category] || category;
 
     // Build the name
     let newName = newCat;
 
-    if (type && type !== 'base') {
-      newName += `__${type}`
+    if (type && type !== "base") {
+      newName += `__${type}`;
     }
 
-    if (item && item !== 'base') {
-      newName += `-${item}`
+    if (item && item !== "base") {
+      newName += `-${item}`;
     }
 
     if (subitem && subitem) {
-      newName += `_${subitem}`
+      newName += `_${subitem}`;
     }
 
     if (state) {
-      newName += `--${state}`
+      newName += `--${state}`;
     }
 
-    return newName
+    return newName;
   }
-})
+});
 
 StyleDictionary.registerTransform({
-  name: 'size/pxToRem',
-  type: 'value',
+  name: "size/pxToRem",
+  type: "value",
   matcher: prop =>
-    prop.attributes.category === 'size' ||
-    prop.attributes.category === 'layout' ||
-    prop.attributes.category === 'font-size' ||
-    prop.attributes.category === 'line-height' ||
-    prop.attributes.category === 'spacing' ||
-    prop.attributes.category === 'border-radius' ||
-    prop.attributes.category === 'components' && prop.attributes.item === 'border-radius' ||
-    prop.attributes.category === 'components' && prop.attributes.item === 'width',
+    prop.attributes.category === "size" ||
+    prop.attributes.category === "layout" ||
+    prop.attributes.category === "font-size" ||
+    prop.attributes.category === "line-height" ||
+    prop.attributes.category === "spacing" ||
+    prop.attributes.category === "border-radius" ||
+    (prop.attributes.category === "components" &&
+      prop.attributes.item === "border-radius") ||
+    (prop.attributes.category === "components" &&
+      prop.attributes.item === "width"),
   transformer: prop => {
     if (prop.original.value === 0) {
-      return "0"
+      return "0";
     }
 
-    return (parseInt(prop.original.value) / 16).toString() + 'rem'
+    return (parseInt(prop.original.value) / 16).toString() + "rem";
   }
-})
+});
 
 StyleDictionary.registerTransform({
-  name: 'font/family/css',
-  type: 'value',
-  matcher: prop => prop.attributes.category === 'font-family' || (prop.attributes.category === 'font' && prop.attributes.type === 'family'),
-  transformer: prop => prop.original.fallback ? prop.original.value + ', ' + prop.original.fallback : prop.original.value
-})
+  name: "font/family/css",
+  type: "value",
+  matcher: prop =>
+    prop.attributes.category === "font-family" ||
+    (prop.attributes.category === "font" && prop.attributes.type === "family"),
+  transformer: prop =>
+    prop.original.fallback
+      ? prop.original.value + ", " + prop.original.fallback
+      : prop.original.value
+});
+
+StyleDictionary.registerTransform({
+  name: "name/figma",
+  type: "name",
+  transformer: prop => {
+    let name = prop.path;
+    let newName = name
+      .map(word => `${word.charAt(0).toUpperCase()}${word.slice(1)}`)
+      .join(" / ");
+    return newName;
+  }
+});
+
+StyleDictionary.registerTransform({
+  name: "color/figma",
+  type: "value",
+  matcher: prop => {
+    const matches = ["color", "text-color", "background-color", "border-color"];
+    return matches.indexOf(prop.attributes.category) > -1;
+  },
+  transformer: prop => {
+    // Transform this into a Figma value
+    const color = tinycolor(prop.value);
+    const colorRgb = color.toRgb();
+    const { r, g, b, a } = colorRgb;
+    const figmaPaint = {
+      opacity: a,
+      color: {
+        r: r / 255,
+        g: g / 255,
+        b: b / 255
+      }
+    };
+    const figmaPaintStyle = {
+      paints: [
+        {
+          type: "SOLID",
+          visible: true,
+          ...figmaPaint
+        }
+      ]
+    };
+    return figmaPaintStyle;
+  }
+});
 
 /**
  * Transform Groups
  */
 StyleDictionary.registerTransformGroup({
-  name: 'heartwood/scss',
+  name: "heartwood/scss",
   transforms: [
-    'attribute/cti', 'name/bem', 'time/seconds', 'content/icon', 'size/pxToRem', 'font/family/css', 'color/css', 'asset/base64'
+    "attribute/cti",
+    "name/bem",
+    "time/seconds",
+    "content/icon",
+    "size/pxToRem",
+    "font/family/css",
+    "color/css",
+    "asset/base64"
   ]
-})
+});
 
 StyleDictionary.registerTransformGroup({
-  name: 'heartwood/js-scss',
-  transforms: ['attribute/cti', 'name/bem', 'size/px', 'color/hex']
-})
+  name: "heartwood/js-scss",
+  transforms: ["attribute/cti", "name/bem", "size/px", "color/hex"]
+});
+
+StyleDictionary.registerTransformGroup({
+  name: "figma",
+  transforms: ["attribute/cti", "name/figma", "color/figma"]
+});
 
 /**
  * Formats
  */
 StyleDictionary.registerFormat({
-  name: 'scss/defaults',
-  formatter: function (dictionary) {
-    return fileHeader(this.options) + variablesWithPrefix('$', dictionary.allProperties);
+  name: "scss/defaults",
+  formatter: function(dictionary) {
+    return (
+      fileHeader(this.options) +
+      variablesWithPrefix("$", dictionary.allProperties)
+    );
   }
-})
+});
 
 StyleDictionary.registerFormat({
-  name: 'scss/map-multi',
-  formatter: function (dictionary) {
-    return sassMultiMap({ mapPrefix: this.mapPrefix || '', properties: dictionary.allProperties });
+  name: "scss/map-multi",
+  formatter: function(dictionary) {
+    return sassMultiMap({
+      mapPrefix: this.mapPrefix || "",
+      properties: dictionary.allProperties
+    });
   }
-})
+});
 
 StyleDictionary.registerFormat({
-  name: 'scss/theme',
+  name: "scss/theme",
   formatter: dictionary => `
 /**
  * Edit values to override the base theme
  */
 
-${dictionary.allProperties.map(prop => `$${prop.name}: ${prop.value};`).join('\n')}
+${dictionary.allProperties
+  .map(prop => `$${prop.name}: ${prop.value};`)
+  .join("\n")}
 `
-})
+});
 
 StyleDictionary.registerFormat({
-  name: 'json/flat-dash',
-  formatter: function (dictionary) {
-    return '{\n' + _.map(dictionary.allProperties, function (prop) {
-      return `  "${prop.path.join('-')}": ${JSON.stringify(prop.value)}`;
-    }).join(',\n') + '\n}';
+  name: "json/flat-dash",
+  formatter: function(dictionary) {
+    return (
+      "{\n" +
+      _.map(dictionary.allProperties, function(prop) {
+        return `  "${prop.path.join("-")}": ${JSON.stringify(prop.value)}`;
+      }).join(",\n") +
+      "\n}"
+    );
   }
-})
+});
 
 StyleDictionary.registerFormat({
-  name: 'figma/color',
-  formatter: function (dictionary) {
-    return '{\n' + _.map(dictionary.allProperties, function (prop) {
-      const color = tinycolor(prop.value)
-      const colorRgb = color.toRgb();
-      const { r, g, b, a } = colorRgb
-      const figmaPaint = {
-        opacity: a,
-        color: {
-          r: r / 255,
-          g: g / 255,
-          b: b / 255
-        }
-      }
-      const figmaPaintStyle = {
-        paints: [
-          {
-            type: 'SOLID',
-            visible: true,
-            ...figmaPaint
-          }
-        ]
-      }
-      if (color.isValid()) {
-        return `  "${prop.path.join(' / ')}": ${JSON.stringify(figmaPaintStyle)}`;
-      }
-    }).join(',\n') + '\n}';
+  name: "figma/color",
+  formatter: dictionary => {
+    return (
+      "{\n" +
+      dictionary.allProperties
+        .map(prop => `"${prop.name}": ${JSON.stringify(prop.value, null, 2)}`)
+        .join(",\n") +
+      "\n"
+    );
   }
-})
+});
 
 /**
  * Filters
  */
 StyleDictionary.registerFilter({
-  name: 'omit',
-  matcher: prop => prop.attributes.category !== 'size'
-})
+  name: "omit",
+  matcher: prop => prop.attributes.category !== "size"
+});
 
 StyleDictionary.registerFilter({
-  name: 'tokens',
-  matcher: prop => prop.attributes.category !== 'components' && prop.attributes.category !== 'asset'
-})
+  name: "tokens",
+  matcher: prop =>
+    prop.attributes.category !== "components" &&
+    prop.attributes.category !== "asset"
+});
 
 StyleDictionary.registerFilter({
-  name: 'tokens/android',
-  matcher: prop => prop.attributes.category === 'color' || (prop.attributes.category === 'size' && prop.attributes.type === 'font')
-})
+  name: "tokens/android",
+  matcher: prop =>
+    prop.attributes.category === "color" ||
+    (prop.attributes.category === "size" && prop.attributes.type === "font")
+});
 
 StyleDictionary.registerFilter({
-  name: 'tokens/ios',
-  matcher: prop => prop.attributes.category === 'color' || (prop.attributes.category === 'size' && prop.attributes.type === 'font')
-})
+  name: "tokens/ios",
+  matcher: prop =>
+    prop.attributes.category === "color" ||
+    (prop.attributes.category === "size" && prop.attributes.type === "font")
+});
 
 StyleDictionary.registerFilter({
-  name: 'components',
-  matcher: prop => prop.attributes.category === 'components'
-})
+  name: "components",
+  matcher: prop => prop.attributes.category === "components"
+});
+
+StyleDictionary.registerFilter({
+  name: "figma/shared-styles",
+  matcher: prop => {
+    const matches = ["color", "text-color", "background-color", "border-color"];
+    return matches.indexOf(prop.attributes.category) > -1;
+  }
+});
 
 /**
  * Build command
  */
-StyleDictionary.buildAllPlatforms()
+StyleDictionary.buildAllPlatforms();
