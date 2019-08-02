c7c8eac2b17d52e58ec94f1fbdd43d6400adc5fb
jtini <jeremytinianow@Jeremys-MacBook-Pro-2.local>
2019-08-02 13:21:51 -0600

    update intro to figma platform tokens

--- src/pages/tokens/figma.tsx
+++ src/pages/tokens/figma.tsx
@@ -3,22 +3,25 @@ import TokensLayout from "../../layouts/TokensLayout/TokensLayout";
 import Card from "../../components/Card/Card";
 import tokensFigma from "../../../build/js/tokens-figma";
 import figmaIcon from "../../icons/figma-icon.svg";
-import './figma.scss'
+import "./figma.scss";
 
 const FigmaPage = (): React.ReactElement => (
   <TokensLayout title="Global Tokens" platform="figma" tokens={tokensFigma}>
-    <a
-      href="https://www.figma.com/file/ByFnA60kWEES79qherfepN/Heartwood-Tokens?node-id=0%3A1"
-      target="_blank"
-      className="figma-tokens__card"
-    >
-      <Card icon={{ src: figmaIcon, width: 24, alt: "Figma Logo" }}>
-        <>
-          <p className="fw-bold">Heartwood Tokens</p>
-          <p>Open the latest Heartwood Tokens file in Figma</p>
-        </>
-      </Card>
-    </a>
+    <>
+      <h2 className="figma-tokens__resources-headline">Resources</h2>
+      <a
+        href="https://www.figma.com/file/ByFnA60kWEES79qherfepN/Heartwood-Tokens?node-id=0%3A1"
+        target="_blank"
+        className="figma-tokens__card"
+      >
+        <Card icon={{ src: figmaIcon, width: 24, alt: "Figma Logo" }}>
+          <>
+            <p className="fw-bold">Heartwood Tokens</p>
+            <p>Open the latest Heartwood Tokens file in Figma</p>
+          </>
+        </Card>
+      </a>
+    </>
   </TokensLayout>
 );
 
