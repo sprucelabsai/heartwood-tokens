0fa92311cb2f8538f31a1ae321df1be71a2dd37c
jtini <jeremytinianow@Jeremys-MacBook-Pro-2.local>
2019-08-06 14:16:47 -0600

    add usage instructions to scss and javascript platforms

--- src/pages/tokens.tsx
+++ src/pages/tokens.tsx
@@ -1,9 +1,23 @@
 import React from "react";
 import TokensLayout from "../layouts/TokensLayout/TokensLayout";
+import CodeSnippet from "../components/CodeSnippet/CodeSnippet";
 import tokensScss from "../../build/js/tokens-scss";
+import './tokens.scss'
 
 const TokensPage = (): React.ReactElement => (
-  <TokensLayout title="Global Tokens" platform="scss" tokens={tokensScss} />
+  <TokensLayout title="Global Tokens" platform="scss" tokens={tokensScss}>
+    <div className="tokens-usage">
+      <h2 className="h3 tokens-usage__title">Usage</h2>
+      <p className="tokens-usage__instruction">
+        <strong className="tokens-usage__label">Install: </strong>{" "}
+        <CodeSnippet text="yarn add @sprucelabs/heartwood-tokens" />
+      </p>
+      <p className="tokens-usage__instruction">
+        <strong className="tokens-usage__label">Import: </strong>{" "}
+        <CodeSnippet text={`@import '@sprucelabs/heartwood-tokens/build/scss/tokens';`} />
+      </p>
+    </div>
+  </TokensLayout>
 );
 
 export default TokensPage;
--- src/pages/tokens/javascript.tsx
+++ src/pages/tokens/javascript.tsx
@@ -1,7 +1,22 @@
 import React from "react";
 import TokensLayout from "../../layouts/TokensLayout/TokensLayout";
+import CodeSnippet from "../../components/CodeSnippet/CodeSnippet";
 import tokensJs from "../../../build/js/tokens";
 
-const JavascriptPage = (): React.ReactElement => <TokensLayout title="Global Tokens" platform="javascript" tokens={tokensJs} />;
+const JavascriptPage = (): React.ReactElement => (
+  <TokensLayout title="Global Tokens" platform="javascript" tokens={tokensJs}>
+    <div className="tokens-usage">
+      <h2 className="h3 tokens-usage__title">Usage</h2>
+      <p className="tokens-usage__instruction">
+        <strong className="tokens-usage__label">Install: </strong>{" "}
+        <CodeSnippet text="yarn add @sprucelabs/heartwood-tokens" />
+      </p>
+      <p className="tokens-usage__instruction">
+        <strong className="tokens-usage__label">Import: </strong>{" "}
+        <CodeSnippet text={`import tokens from "@sprucelabs/heartwood-tokens/build/js/tokens"`}/>
+      </p>
+    </div>
+  </TokensLayout>
+);
 
-export default JavascriptPage
+export default JavascriptPage;
