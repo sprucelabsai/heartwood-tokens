203991ae7ac7f5fdd8e779b8106964a0a5efd9fe
jtini <jeremytinianow@Jeremys-MacBook-Pro-2.local>
2019-08-07 11:39:15 -0600

    fix og:title

--- src/components/SEO/SEO.tsx
+++ src/components/SEO/SEO.tsx
@@ -34,7 +34,7 @@ function SEO({ description, lang, meta, title }) {
         },
         {
           property: `og:title`,
-          content: name
+          content: title
         },
         {
           property: `og:description`,
